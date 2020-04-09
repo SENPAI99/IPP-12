fdc4c00341cddc5cc5d7bdf56e708c93eb25d355
SENPAI99 <60696293+SENPAI99@users.noreply.github.com>
2020-04-09 11:30:25 +0200

    Delete Uppgift_1_1_1.py

--- Uppgift_1_1_1.py
+++
@@ -1,8 +0,0 @@
-#! python3 
-# filnamn: Uppgift1_1_1.py 
-# Skrivet av: Mustafa Mohammedi 
-# Skapat datum: 2019-10-02 
-# Senast ��ndrat: 2019-10-02 
-# Kurs: Grundl��ggande Datavetenskap 
-#  Handledare: Lars ... 
-#  Beskrivning: Programmet skriver ut . . . .
